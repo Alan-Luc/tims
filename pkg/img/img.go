74ab06fb686a86ae07bc8cdf06b205bb0e60ea01
Alan-Luc <alanluc9@gmail.com>
2023-01-31 15:32:40 -0500

    add demo gif

--- pkg/img/img.go
+++ pkg/img/img.go
@@ -77,7 +77,6 @@ func (i *Images) Scale(ind int) {
 	output := fmt.Sprintf("%s%s@2x.png", outputDir, img.FileName)
 
 	cmd := exec.Command("waifu2x-ncnn-vulkan", "-i", img.FilePath, "-o", output, "-n", "2")
-	fmt.Println(cmd)
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
